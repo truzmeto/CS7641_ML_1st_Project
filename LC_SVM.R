c47b8e5bfee7ae8424df6d28b254f11b44adc681
Talant Ruzmetov <truzmeto@kent.edu>
2017-09-20 13:57:30 -0400

    svm_learning_curve

--- LC_SVM.R
+++ LC_SVM.R
@@ -51,3 +51,92 @@ pdf("figs/svm_acc_cost_sigma.pdf")
 plot(model_svm)
 dev.off()
 
+
+
+##-------------------------------- Experiment 2 -------------------------------
+# Learning Curve
+# Vary trainig set size and and observe how accuracy of prediction affected
+
+N_iter <- 10  #|> number of iterations for learning curve
+
+# initilzing empty array for some measures
+test_accur <- 0
+test_kap <- 0
+train_accur <- 0
+train_kap <- 0
+
+cpu_time <- 0
+data_size <- 0
+
+set.seed(500)   #|> setting random seed
+train_frac <- 0.8
+
+training1 <- training
+
+TrainCtrl <- trainControl(method = "cv")
+SVMgrid <- expand.grid(sigma = c(0.033), C = 1.5)
+
+
+for (i in 1:N_iter) { 
+  
+  
+  new_train <- training1 
+  training1 <- new_train[createDataPartition(y = new_train$loan_status, p = 0.8, list = FALSE),]
+  validation1 <- training[createDataPartition(y = training1$loan_status, p = 0.3, list = FALSE), ]
+  
+  
+  start_time <- Sys.time() ## start the clock------------------------------------------------------
+  svmFit <- train(factor(loan_status) ~ .,
+                     data = training, 
+                     method="svmRadial",
+                     trControl=TrainCtrl,
+                     tuneGrid = SVMgrid,
+                     preProc = c("scale","center"),
+                     verbose=FALSE)
+  
+  svmFit
+  end_time <- Sys.time()  ## end the clock---------------------------------------------------------
+  
+  
+  ## making a prediction
+  prediction_svm_test <- predict(svmFit, testing, type = "raw")
+  con_mat_test <- confusionMatrix(prediction_svm_test, testing$loan_status)
+  
+  prediction_svm_train <- predict(svmFit, validation1, type = "raw")
+  con_mat_train <- confusionMatrix(prediction_svm_train, validation1$loan_status)
+  
+  cpu_time[i] <- as.numeric(end_time - start_time)
+  data_size[i] <- nrow(training1)
+  
+  test_accur[i] <- as.numeric(con_mat_test$overall[1])
+  test_kap[i] <- as.numeric(con_mat_test$overall[2])
+  
+  train_accur[i] <- as.numeric(con_mat_train$overall[1])
+  train_kap[i] <- as.numeric(con_mat_train$overall[2])
+}
+
+results <- data.frame(test_accur,test_kap,train_accur,train_kap, cpu_time, data_size)
+
+
+## plot some results
+pl <- ggplot(results, aes(x=data_size)) +
+  geom_line(aes(y = train_accur, colour = "train")) + 
+  geom_line(aes(y = test_accur, colour = "test")) +
+  geom_point(aes(y = train_accur,colour = "train")) + 
+  geom_point(aes(y = test_accur,colour = "test")) +
+  theme_bw() +
+  labs(title = "Learning Curve SVM", x = "Training Size", y = "Accuracy", color="") +
+  theme(legend.position = c(0.6,0.8),
+        axis.title = element_text(size = 16.0),
+        axis.text = element_text(size=10, face = "bold"),
+        plot.title = element_text(size = 15, hjust = 0.5),
+        axis.text.x = element_text(colour="black"),
+        axis.text.y = element_text(colour="black"))
+
+#plot and save
+png("figs/svm_learning_curve.png", width = 5.0, height = 4.0, units = "in", res = 800)
+pl
+dev.off()
+
+
+
