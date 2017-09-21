622aed8d44dd3b01dbd9dee35f51dff7ba23a67e
Talant Ruzmetov <truzmeto@kent.edu>
2017-09-21 11:32:04 -0400

    crosVal

--- LC_nnet.R
+++ LC_nnet.R
@@ -5,6 +5,9 @@ library("ggplot2")
 library("lattice") 
 library("caret")
 library("plyr")
+library(doMC)
+registerDoMC(cores = 4)
+
 
 ## setting seed for random number generator
 set.seed(300)
@@ -15,7 +18,7 @@ data[data$loan_status == 0,]$loan_status <- "No"
 data[data$loan_status == 1,]$loan_status <- "Yes"
 
 ## extract 10% of data and perfor hyperparameter tuning 
-sub_data <- data[createDataPartition(y=data$loan_status, p = 0.05, list=FALSE),]
+sub_data <- data[createDataPartition(y=data$loan_status, p = 0.1, list=FALSE),]
 
 # break sub data into train test and validation sets
 indx <- createDataPartition(y=sub_data$loan_status, p = 0.70, list=FALSE)
@@ -29,12 +32,12 @@ testing <- sub_data[-indx, ]
 ## Neural Network Model
 set.seed(2017)
 fitControl <- trainControl(method = "repeatedcv", 
-                           number = 10, 
-                           repeats = 5, 
+                           number = 5, 
+                           repeats = 2, 
                            classProbs = TRUE, 
                            summaryFunction = twoClassSummary)
 
-nnetGrid <- expand.grid(size = seq(from = 1, to = 5, by = 1),
+nnetGrid <- expand.grid(size = seq(from = 5, to = 15, by = 1),
                          decay = seq(from = 0.1, to = 0.5, by = 0.1))
 
 nnetFit <- train(factor(loan_status) ~ .,
@@ -45,7 +48,7 @@ nnetFit <- train(factor(loan_status) ~ .,
                  tuneGrid = nnetGrid,
                  verbose = FALSE)
 
-prediction_nnet <- predict(nnetFit, newdata=testing, type = "class")
+prediction_nnet <- predict(nnetFit, newdata=testing, type = "raw")
 con_mat_nnet <- confusionMatrix(prediction_nnet, testing$loan_status)
 con_mat_nnet$overall[1]
-
+plot(nnetFit)
